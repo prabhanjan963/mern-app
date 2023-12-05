121000215b4a98b17269ecf9252d48ac948d8997
Prabhanjan <prabhanjan250@gmail.com>
2023-12-05 20:30:46 +0530

    mern updated

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,20 @@ import express from 'express'
 import cors from 'cors'
 import routes from './routes/router.js';
 import dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config()
+
+const __dirname = path.resolve();
+
 const app = express()
 const PORT = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname,"/client/dist")))
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname,"client","dist","index.html"))
+})
 
 app.use(express.json())
 app.use(cors())
